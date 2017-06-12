ec784f187ef9c000cbb380273ecfd79b2b4b2762
mohammad sakr <mohammad_sakr_98@yahoo.com>
2017-06-13 01:01:49 +0200

    Updates

---
+++ IdentityProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityProject.Startup))]
+namespace IdentityProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
